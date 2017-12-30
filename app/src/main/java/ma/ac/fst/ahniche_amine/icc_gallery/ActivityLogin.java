93723d24a11a63ad9f771c47b0d719bdac5e4de8
Amine Ahniche <amineahniche@gmail.com>
2017-12-30 13:04:17 +0000

    First commit

---
+++ app/src/main/java/ma/ac/fst/ahniche_amine/icc_gallery/ActivityLogin.java
@@ -0,0 +1,14 @@
+package ma.ac.fst.ahniche_amine.icc_gallery;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ActivityLogin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
